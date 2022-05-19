d39fa3ee64cdd70e13a0d9cc858e1ec51569b660
BakelD <bakeld85@gmail.com>
2022-05-19 20:02:00 +0300

    Done task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnEl = document.querySelector('.change-color');
+
+changeColorBtnEl.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+});
