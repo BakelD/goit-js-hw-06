103ccff84390146233fb480e9a594d6262208d2b
BakelD <bakeld85@gmail.com>
2022-05-19 19:54:50 +0300

    Done task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector('form.login-form');
+const emailEl = formEl.querySelector('[name="email"]');
+const passwordEl = formEl.querySelector('[name="password"]');
 
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = emailEl.value;
+  const password = passwordEl.value;
+
+  if (!email || !password) {
+    alert('all fields must be filled in');
+    return;
+  }
+
+  displayData({ email, password });
+
+  emailEl.value = '';
+  passwordEl.value = '';
+});
+
+function displayData(obj) {
+  for (const prop in obj) {
+    console.log(`${prop} -> ${obj[prop]}`);
+  }
+}
