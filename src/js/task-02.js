1f54c30544512e2f645031cfc4fe44dc0d51e66b
BakelD <bakeld85@gmail.com>
2022-05-19 17:32:31 +0300

    Done task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const arrListEl = ingredients.map((el) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = el;
+  listItemEl.classList.add("item");
+
+  return listItemEl;
+});
+
+ingredientsEl.append(...arrListEl);
